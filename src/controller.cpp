107fabc7a351caf0031bea0d526f65c92b41041f
bryan calcagno <bryan.a.calcagno@lmco.com>
2022-08-26 19:32:07 +0000

    'added multiplayer

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,72 +3,26 @@
 #include "SDL.h"
 #include "snake.h"
 
-void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
-                                 Snake::Direction opposite) const {
+void Controller::ChangeDirection(Snake &snake, Snake::Direction input, Snake::Direction opposite) const {
   if (snake.direction != opposite || snake.size == 1) snake.direction = input;
   return;
 }
 
-void Controller::HandleInput1(bool &running, Snake &snake) const {
+void Controller::HandleInput(bool &running, Snake &snake) const {
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
       running = false;
     } else if (e.type == SDL_KEYDOWN) {
-      switch (e.key.keysym.sym) {
-        case SDLK_UP:
-          ChangeDirection(snake, Snake::Direction::kUp,
-                          Snake::Direction::kDown);
-          break;
-
-        case SDLK_DOWN:
-          ChangeDirection(snake, Snake::Direction::kDown,
-                          Snake::Direction::kUp);
-          break;
-
-        case SDLK_LEFT:
-          ChangeDirection(snake, Snake::Direction::kLeft,
-                          Snake::Direction::kRight);
-          break;
-
-        case SDLK_RIGHT:
-          ChangeDirection(snake, Snake::Direction::kRight,
-                          Snake::Direction::kLeft);
-          break;
-      }
+      
+      if(e.key.keysym.sym == _Up)
+          ChangeDirection(snake, Snake::Direction::kUp,Snake::Direction::kDown);
+      else if(e.key.keysym.sym == _Down)
+          ChangeDirection(snake, Snake::Direction::kDown,Snake::Direction::kUp);
+      else if(e.key.keysym.sym == _Left)
+          ChangeDirection(snake, Snake::Direction::kLeft,Snake::Direction::kRight);
+      else
+          ChangeDirection(snake, Snake::Direction::kRight, Snake::Direction::kLeft);
     }
   }
 }
-
-
-void Controller::HandleInput2(bool &running, Snake &snake) const {
-  SDL_Event e;
-  while (SDL_PollEvent(&e)) {
-    if (e.type == SDL_QUIT) {
-      running = false;
-    } else if (e.type == SDL_KEYDOWN) {
-      switch (e.key.keysym.sym) {
-        case SDLK_w:
-          ChangeDirection(snake, Snake::Direction::kUp,
-                          Snake::Direction::kDown);
-          break;
-
-        case SDLK_s:
-          ChangeDirection(snake, Snake::Direction::kDown,
-                          Snake::Direction::kUp);
-          break;
-
-        case SDLK_a:
-          ChangeDirection(snake, Snake::Direction::kLeft,
-                          Snake::Direction::kRight);
-          break;
-
-        case SDLK_d:
-          ChangeDirection(snake, Snake::Direction::kRight,
-                          Snake::Direction::kLeft);
-          break;
-      }
-    }
-  }
-}
-
